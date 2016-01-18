3c36ada497326a1a44b7cbd4ba3b9c90c72ad674
phytrisha <markf.meyer@icloud.com>
2016-01-18 23:28:36 +0100

    -filled album database

--- designV1/json/albums.js
+++ designV1/json/albums.js
@@ -70,6 +70,222 @@ getAlbums([
 		"artist":"Taylor Swift",
 		"song":["Welcome To New York", "Blank Space", "Style", "Out of the Woods", "All You Had To Do Was Stay", "Shake It Off", "I Wish You Would", "Bad Blood", "Wildest Dreams", "How You Get the Girl", "This Love", "I Know Places", "Clean"],
 		"length":["3:32","3:51","3:51","3:55","3:13","3:39","3:27","3:31","3:40","4:07","4:10","3:15","4:31"]
+	},
+	{
+		"name":"Hot Fuss",
+		"artist":"The Killers",
+		"song":["Jenny Was a Friend of Mine", "Mr. Brightside", "Smile Like You Mean It", "Somebody Told Me", "All These Things That I've Done", "Andy, You're a Star", "On Top", "Change Your Mind", "Believe Me Natalie", "Midnight Show", "Everything Will Be Alright", "Glamorous Indie Rock & Roll"],
+		"length":["4:04", "3:42", "3:55", "3:17", "5:01", "3:14", "4:18", "3:11", "5:04", "4:02", "5:45", "4:14"]
+	},
+	{
+		"name":"21",
+		"artist":"Adele",
+		"song":["Rolling In the Deep", "Rumour Has It", "Turning Tables", "Don't You Remember", "Set Fire to the Rain", "He Won't Go", "Take It All", "I'll Be Waiting", "One and Only", "Lovesong", "Someone Like You", "I Found a Boy (Bonus Track)"],
+		"length":["3:48", "3:43", "4:10", "4:03", "4:02", "4:38", "3:48", "4:01", "5:48", "5:16", "4:45", "3:37"]
+	},
+	{
+		"name":"The Lateness of the Hour",
+		"artist":"Alex Clare",
+		"song":["Up All Night", "Treading Water", "Relax My Beloved", "Too Close", "When Doves Cry", "Humming Bird", "Hands Are Clever", "Tight Rope", "Whispering", "I Love You", "Sanctuary", "I Won't Let You Down"],
+		"length":["2:43", "3:38", "3:31", "4:16", "4:08", "3:50", "3:16", "3:37", "4:30", "4:39", "4:00", "4:09"]
+	},
+	{
+		"name":"This Is the Life",
+		"artist":"Amy Macdonald",
+		"song":["Mr. Rock & Roll", "This Is the Life", "Poison Prince", "Youth of Today", "Run", "Let's Start a Band", "Barrowland Ballroom", "L.A.", "A Wish for Something More", "Footballer's Wife", "The Road to Home", "Poison Prince (Acoustic W14 Session", "Run (Acoustic W14 Session)", "Caledonia"],
+		"length":["3:34", "3:04", "3:28", "3:58", "3:48", "4:04", "3:55", "4:05", "3:44", "5:05", "2:22", "2:40", "3:59", "2:06"]
+	},
+	{
+		"name":"Megalithic Symphony",
+		"artist":"AWOLNATION",
+		"song":["Megalithic Symphony", "Some Sort of Creature", "Soul Wars", "People", "Jump On My Shoulders", "Burn It Down", "Guilty Filthy Soul", "Kill Your Heroes", "My Nightmare's Dream", "Sail", "Wake Up", "Not Your Fault", "All I Need", "Knights of Shame", "Swinging from the Castles"],
+		"length":["0:57", "0:26", "3:37", "3:58", "4:08", "2:45", "3:33", "2:58", "0:26", "4:19", "3:02", "4:02", "3:37", "14:58", "3:04"] 
+	},
+	{
+		"name":"One Day (Vandaag)",
+		"artist":"Bakermat",
+		"song":["One Day (Vandaag) [Radio Edit]", "One Day (Vandaag)", "One Day (Vandaag) [FlicFlac Remix]", "One Day (Vandaag) [Anime Edge & DANCE Remix]", "One Day (Vandaag) [Plastik Funk Remix]", "One Day (Vandaag) [Instrumental]"],
+		"length":["3:39", "5:23", "6:13", "6:16", "6:01", "5:23"]
+	},
+	{
+		"name":"Billy Talent II",
+		"artist":"Billy Talent",
+		"song":["Devil In a Midnight Mass", "Red Flag", "This Suffering", "Worker Bees", "Pins and Needles", "Fallen Leaves", "Where Is the Line?", "Covered In Cowardice", "Surrender", "The Navy Song", "Perfect World", "Sympathy", "Burn the Evidence", "Ever Fallen In Love (With Someone You Shouldn'tve?)"],
+		"length":["2:51", "3:18", "3:56", "3:44", "3:11", "3:19", "3:49", "4:12", "4:06", "4:31", "3:06", "3:18", "3:40", "2:45"]
+	},
+	{
+		"name":"Papillon",
+		"artist":"Butch",
+		"song":["The Table", "About This Time", "Wet'n'tight", "Amelie", "Ein - E", "Osiris", "Jazzy Belle", "Kaleidoscope Eyes", "Bad Weed", "Soulatan (feat. Julie Marghilano)", "Gag Ball Roll", "Something Called Acid", "Sand"],
+		"length":["5:57", "4:51", "4:36", "6:07", "6:11", "4:41", "4:41", "4:16", "3:20", "5:18", "4:20", "4:11", "8:07"]
+	},
+	{
+		"name":"18 Months",
+		"artist":"Calvin Harris",
+		"song":["Green Valley", "Bounce (feat. Kelis) [Radio Edit]", "Feel So Close (Radio Edit)", "We Found Love (feat. Rihanna)", "We'll Be Coming Back (feat. Example)", "Mansion", "Iron (feat. Nicky Romero)", "I Need Your Love (feat. Ellie Goulding)", "Drinking from the Bottle (feat. Tinie Tempah)", "Sweet Nothing (feat. Florence Welch)", "School", "Here 2 China (feat. Dizzee Rascal)", "Let's Go (feat. Ne-Yo)", "Awooga", "Thinking About You (feat. Ayah Marar)"],
+		"length":["1:49", "3:42", "3:26", "3:35", "3:54", "2:07", "3:39", "3:54", "4:00", "3:32", "1:47", "2:32", "3:52", "3:51", "4:07"]
+	},
+	{
+		"name":"Der Druck steigt (Live)",
+		"artist":"Casper",
+		"song":["Der Druck steigt (Die Vergessenen, Pt.1) [Live]", "Blut sehen (Die Vergessenen, Pt.2) [Live]", "Casper! Bumayé (Live)", "Auf und Davon (Live)", "XOXO (feat. Thees Uhlmann) [Live]", "Unzerbrechlich (Live)", "Alaska (Live)", "Die letzte Gang der Stadt (Live)", "Rock 'n' Roll (Live)", "Medley (Live)", "Lilablau (Live)", "Hin zur Sonne (Live)", "Kreis (Live)", "Das Grizzly Lied (Live)", "Michael X (Live)", "Arlen Griffey (Prelude) [Live]", "Kontrolle/Schlaf (Live)", "So Perfekt (feat. Marteria) [Live]"],
+		"length":["4:03", "2:02", "3:41", "4:17", "4:06", "3:37", "4:40", "3:25", "4:12", "4:20", "3:43", "3:01", "4:06", "4:21", "5:39", "0:57", "5:09", "7:08"]
+	},
+	{
+		"name":"Another World - EP",
+		"artist":"The Chemical Brothers",
+		"song":["Another World (Radio Edit)", "Swoon (Boyz Noize Summer Remix)", "Swoon (Lindstrøm & Prins Thomas Remix)", "Horse Power (Popof Remix)"],
+		"length":["2:57", "5:21", "9:27", "6:34"]
+	},
+	{
+		"name":"Mylo Xyloto",
+		"artist":"Coldplay",
+		"song":["Mylo Xyloto", "Hurts Like Heaven", "Paradise", "Charlie Brown", "Us Against the World", "M.M.I.X.", "Every Teardrop Is a Waterfall", "Major Minus", "U.F.O.", "Princess of China", "Up in Flames", "A Hopeful Transmission", "Don't Let It Break Your Heart", "Up With the Birds"],
+		"length":["0:42", "4:02", "4:37", "4:45", "3:59", "0:48", "4:00", "3:30", "2:17", "3:59", "3:13", "0:33", "3:54", "3:45"]
+	},
+	{
+		"name":"Alive 2007",
+		"artist":"Daft Punk",
+		"song":["Robot Rock / Oh Yeah", "Touch It / Technologic", "Television Rules the Nation / Crescendolls", "Too Long / Steam machine", "Around the World / Harder Better Faster Stronger", "Burnin' / Too Long", "Face to Face / Short Circuit", "One More Time / Aerodynamic", "Aerodynamic Beats / Gabrielle, Forget About the World", "Prime Time of Your Life / Brainwasher / Rollin 'and Scratchin' / Alive", "Da Funk / Dadftendirekt", "Superheroes / Human After All / Rock 'n Roll", "Human / Together / One More Time / Music Sounds Better with You / Stardust (Instrumental) [Live] [Bonus Track]"],
+		"length":["6:27", "5:29", "4:50", "7:01", "5:42", "7:11", "4:55", "6:10", "3:31", "10:22", "6:36", "5:41", "9:58"]
+	},
+	{
+		"name":"4X4=12",
+		"artist":"deadmau5",
+		"song":["4x4=12 (Continuous Mix)", "Some Chords", "Sofi Needs a Ladder", "A City In Florida", "Bad Selection", "Animal Rights", "I Said (Michael Woods Remix)", "Cthulhu Sleeps", "Right This Second", "Raise Your Weapon (Edit)", "One Trick Pony", "Everything Before", "Some Chords (Live from Brixton)", "Cthulhu Sleeps (Live from Brixton)", "Bad Selection (Live from Brixton)", "Right This Second (Live from Brixton)"],
+		"length":["1:09:54", "6:54", "6:43", "5:40", "5:32", "6:15", "7:06", "10:34", "6:50", "6:56", "3:59", "6:36", "3:26", "6:30", "9:22", "4:39"]
+	},
+	{
+		"name":"Control",
+		"artist":"Disclosure",
+		"song":["Control", "Lividup", "Control (Joe Goddard Remix)", "Boiling (Dixon Rework)", "What's In Your Head (Mak & Pasteman Remix)", "Boiling (Medlar Remix)"],
+		"length":["4:49", "6:05", "6:34", "9:31", "4:42", "5:52"]
+	},
+	{
+		"name":"x (Deluxe Edition)",
+		"artist":"Ed Sheeran",
+		"song":["One", "I'm a Mess", "Sing", "Don't", "Nina", "Photograph", "Bloodstream", "Tenerife Sea", "Runaway", "The Man" , "Thinking Out Loud", "Afire Love", "Take It Back", "Shirtsleeves", "Even My Dad Does Sometimes", "I See Fire"],
+		"length":["4:12", "4:04", "3:55", "3:39", "3:45", "4:18", "5:00", "4:01", "3:25", "4:10", "4:41", "5:14", "3:28", "3:10", "3:48", "4:59"]
+	},
+	{
+		"name":"Recovery",
+		"artist":"Eminem",
+		"song":["Cold Wind Blows", "Talkin' 2 Myself", "On Fire", "Won't Back Down", "W.T.P.", "Going Through Changes", "Not Afraid", "Seduction", "No Love", "Space Bound", "Cinderella Man", "25 to Life", "So Bad", "Almost Famous", "Love the Way You Lie", "You're Never Over", "Untitled"],
+		"length":["5:03", "5:00", "3:33", "4:25", "3:58", "4:58", "4:08", "4:35", "4:59", "4:38", "4:39", "4:01", "5:25", "4:52", "4:23", "5:05", "3:14"]
+	},
+	{
+		"name":"Save Rock and Roll",
+		"artist":"Fall Out Boy",
+		"song":["The Phoenix", "My Songs Know What You Did In the Dark (Light Em Up)", "Alone Together", "Where Did the Party Go", "Just One Yesterday (feat. Foxes)", "The Mighty Fall (feat. Big Sean)", "Miss Missing You", "Death Valley", "Young Volcanoes", "Rat a Tat (feat. Courtney Love)", "Save Rock and Roll (feat. Elton John)"],
+		"length":["4:04", "3:06", "3:23", "4:03", "4:04", "3:32", "3:30", "3:46", "3:24", "4:02", "4:41"]
+	},
+	{
+		"name":"Echo - EP",
+		"artist":"Fettes Brot",
+		"song":["Echo", "D Rap Medley (Rock im Park 2013)", "KussKussKuss (Rock im Park 2013)", "Echo (Insturmental)"],
+		"length":["3:31", "4:59", "3:16", "3:30"]
+	},
+	{
+		"name":"Flume",
+		"artist":"Flume",
+		"song":["Sintra","Holdin On", "Left Alone (feat. Chet Faker", "Sleepless (feat. Jezzabell Doran)", "On Top (feat. T-Shirt)", "Stay Close", "Insane (feat. Moon Holiday)", "Change", "Ezra", "More Than You Thought", "Space Cadet", "Bring You Down (feat. George Maple)", "Warm Thoughts", "What You Need", "Star Eyes"],
+		"length":["2:35", "2:34", "3:29", "3:29", "3:51", "2:56", "3:33", "2:30", "3:37", "4:05", "2:12", "4:38", "3:48", "4:10", "2:26"]
+	},
+	{
+		"name":"Interventions and Lullabies",
+		"artist":"The Format",
+		"song":["The First Single (You Know Me)", "Wait, Wait, Wait", "Give It Up", "Tie the Rope", "Tune Out", "I'm Ready, I am", "On Your Porch", "Sore Thumb", "A Mess to Be Made", "Let's Make This Moment a Crime", "Career Day", "A Save Situation"],
+		"length":["4:25", "3:13","3:51","3:19","3:31","3:14","5:11","3:18","3:27","3:49","5:44","2:19"]
+	},
+	{
+		"name":"Fort Atlantic",
+		"artist":"Fort Atlantic",
+		"song":["No One Will Know", "Career Advice", "Let Your Heart Hold Fast", "Up From the Ground", "I'm Wrong", "Movie Screens", "New York Lights", "My Love Is With You", "The Wrecking Ball", "There Is Love"],
+		"length":["4:25", "3:40", "3:46", "4:08", "9:00", "4:36", "3:28", "3:54", "5:59", "3:43"]
+	},
+	{
+		"name":"Channel ORANGE",
+		"artist":"Frank Ocean", 
+		"song":["Start", "Thinkin Bout You", "Fertilizer", "Sierra Leone", "Sweet Life", "Not Just Money", "Super Rich Kids (feat. Earl Sweatshirt)", "Pilot Jones", "Crack Rock", "Pyramids", "Lost", "White (feat. John Mayer)", "Monks", "Bad Religion", "Pink Matter (feat. André 3000)", "Forrest Gump", "End"],
+		"length":["0:45", "3:20", "0:39", "2:28", "4:22", "0:59", "5:04", "3:04", "3:44", "9:52", "3:54", "1:16", "3:20", "2:55", "4:28", "3:14", "2:14"]
+	},
+	{
+		"name":"How to Save a Life",
+		"artist":"The Fray",
+		"song":["She Is", "Over My Head (Cable Car)", "How to Save a Life", "All At Once", "Fall Away", "Heaven Forbid", "Look After You", "Hundred", "Vienna", "Dead Wrong", "Little House", "Trust Me"],
+		"length":["3:56", "3:56", "4:22", "3:47", "4:23", "4:00", "4:26", "4:13", "3:51", "3:05", "2:30", "3:22"]
+	},
+	{
+		"name":"These Things Happen",
+		"artist":"G-Eazy",
+		"song":["These Things Happen", "Far Alone (feat. E-40 & Jay Ant)", "I Mean It (feat. Remo)", "Interlude", "Opportunity Cost", "Almost Famous", "Lotta That (feat. A$AP Ferg & Danny Seth)", "Factory Girl (Skit)" , "Downtown Love (feat. John Michael Rouchell)", "Complete", "Let's Get Lost (feat. Devon Baldwin)", "Shoot Me Down (feat. Anthony Stewart)", "Been On", "Remember You (feat. Blackbear)", "Tumblr Girls (feat. Christoph Andersson)", "Just Believe"],
+		"length":["2:24", "4:27", "3:56", "0:44", "3:54", "4:28", "4:48", "0:13", "5:26", "3:08", "4:00", "3:16", "3:28", "3:35", "4:15", "4:02"]
+	},
+	{
+		"name":"D-Sides",
+		"artist":"Gorillaz",
+		"song":["68 State", "People", "Hongkongaton", "We Are Happy Lnadfill", "Hong Kong", "Highway (Under Construction)", "Rockit", "Bill Murray", "The Swagga", "Murdoc Is God", "Spitting Out the Demons", "Don't Get Lost In Heaven (Original Demo Version)", "Stop the Dams", "DARE (DFA Remix)", "Feel Good Inc (Stanton Warriors Remix)", "Kids With Guns (Jamie T's Turns To Monsters Mix)", "DARE (Soulwax Remix)", "Kids With Guns (Hot Chip Remix)", "El Manana (Metronomy Remix)", "DARE (Junior Sanchez Remix)", "Dirty Harry (Schtung Chinese New Year Remix)", "Kids With Guns (Quiet Village Remix)"],
+		"length":["4:48", "3:27", "3:33", "3:39", "7:14", "4:20", "3:33", "3:52", "4:57", "2:26", "5:10", "2:29", "5:38", "12:14", "7:24", "4:22", "5:42", "7:09", "5:44", "5:26", "3:53", "10:08"]
+	},
+	{
+		"name":"Night Visions",
+		"artist":"Imagine Dragons",
+		"song":["Radioactive", "Tiptoe", "It's Time", "Demons", "On Top of the World", "Amsterdam", "Hear Me", "Every Night", "Bleeding Out", "Underdog", "Nothing Left to Say / Rocks", "Cha-Ching (Till We Grow Older)", "Working Man", "Fallen"],
+		"length":["3:06", "3:14", "4:00", "2:57", "3:12", "4:01", "3:55", "3:37", "3:43", "3:29", "8:59", "4:08", "3:55", "2:59"]
+	},
+	{
+		"name":"Sleep Through the Static",
+		"artist":"Jack Johnson",
+		"song":["All At Once", "Sleep Through the Static", "Hope", "Angel", "Enemy", "If I Had Eyes", "Same Girl", "What You Thought You Need", "Adrift", "Go On", "They Do, They Don't", "While We Wait", "Monsoon", "Losing Keys"],
+		"length":["3:37","3:43","3:42", "2:02", "3:48", "3:58", "2:10", "5:27", "3:56", "4:34", "4:10", "1:26", "4:16", "4:28"]
+	},
+	{
+		"name":"Magna Carta... Holy Grail",
+		"artist":"JAY Z",
+		"song":["Holy Grail (feat. Justin Timberlake)", "Picasso Baby", "Tom Ford", "F*CKWithMeYouKnowIGotIt (feat. Rick Ross)", "Oceans (feat. Frank Ocean)", "F.U.T.W.", "Somewhere In America", "Crown", "Heaven", "Versus", "Part II (On the Run) [feat. Beyoncé]", "Beach Is Better", "BBC", "JAY Z Blue", "La Familia", "Nickels and Dimes"],
+		"length":["5:38", "4:05", "3:09","4:03", "3:58", "4:02","2:28","4:33","4:03","0:51","5:33","0:55","3:12","3:50","3:33", "5:03"]
+	},
+	{
+		"name":"Only By the Night",
+		"artist":"Kings of Leon",
+		"song":["Closer", "Crawl", "Sex On Fire", "Use Somebody", "Manhattan", "Revelry" ,"17", "Notion", "I Want You", "Be Somebody" ,"Cold Desert"],
+		"length":["3:57", "4:06", "3:23", "3:50", "3:24", "3:21","3:05", "3:00", "5:07", "3:47", "5:34"]
+	},
+	{
+		"name":"Rebellion der Träumer",
+		"artist":"Kollektiv Turmstrasse",
+		"song":["Affekt", "Dazwischen 1", "Sphäre", "Dazwischen 2" ,"Kontakt", "Dazwischen 3", "Uneins", "Dazwischen 4", "Deine Distanz", "Dazwischen 5", "Was bleibt", "Dazwischen 6", "Schwindelig", "Dazwischen 7", "Heimat", "Dazwischen 8" ,"Goldmarie", "Dazwischen 9", "Addio Addio"],
+		"length":["4:19", "0:55", "5:29", "0:44", "4:13", "1:06", "5:17", "1:08", "5:58", "1:01", "6:15", "0:52", "4:14", "1:03", "4:03", "1:03", "4:58", "1:37", "5:59"]
+	},
+	{
+		"name":"Born to Die",
+		"artist":"Lana Del Rey",
+		"song":["Born to Die", "Off to the Races" ,"Blue Jeans (Remastered)", "Video Games (Remastered)", "Diet Mountain Dew", "National Anthem", "Dark Paradise", "Radio", "Carmen", "Million Dollar Man", "Summertime Sadness" ,"This Is What Makes Us Girls"],
+		"length":["4:46", "4:59", "3:29", "4:41", "3:42", "3:50", "4:03", "3:34", "4:08", "3:50", "4:24", "4:00"]
+	},
+	{
+		"name":"Lasers",
+		"artist":"Lupe Fiasco",
+		"song":["Letting Go (feat. Sarah Green)", "Words I Never Said (feat. Skylar Grey)", "Till I Get There", "I Don't Wanna Cara Right Now (feat. MDMA)", "Out of My Head (feat. Trey Songz)", "The Show Goes On", "Beautiful Lasers (2 Ways) [feat. MDMA]", "Coming Up (feat. MDMA)", "State Run Radio (feat. sELF)", "Break the Chain (feat. Eric Turner & Sway)", "All Black Everything", "Never Forget You (feat. John Legend)"],
+		"length":["4:26","4:16","3:23","4:15","3:24", "3:56","4:01","3:58","3:57","4:21","3:40","4:04"]
+	},
+	{
+		"name":"Watching Movies With the Sound Off (Deluxe Edition)",
+		"artist":"MAC MILLER",
+		"song":["The Star Room", "Avian", "I'm Not Real (feat. Earl Sweatshirt & Vinny Radio)", "S.D.S.", "Bird Call", "Matches (feat. Ab-Soul)", "I Am Who Am (Killin' Theme) [feat. Niki Randa]", "Objects in the Mirror", "Red Dot Music (feat. Action Bronson)", "Gees (feat. Schoolboy Q)", "Watching Movies", "Suplexes Inside of Complexes and Duplexes", "REMember", "Someone Like You", "Aquarium", "Youforia", "Goosebumpz (Bonus Track)", "O.K. (feat. Tyler, The Creator) [Bonus Track]", "Claymation (feat. Vinny Radio) [Bonus Track]"],
+		"length":["4:35", "3:16", "3:23", "3:04", "2:08", "2:58", "5:01", "4:19", "6:07", "2:55", "3:40", "2:47", "4:28", "4:16", "4:38", "4:01", "3:22", "2:40", "3:52"]
+	},
+	{
+		"name":"The Resistance",
+		"artist":"Muse",
+		"song":["Uprising", "Resistance", "Undisclosed Desires" ,"United States of Eurasia (+Collateral Damage)", "Guiding Light", "Unnatural Selection", "MK Ultra", "I Belong to You (+Mon Coeur S'Ouvre a Ta Voix)" ,"Exogenesis: Symphony, Pt. 1 (Overture)", "Exogenesis: Symphony, Pt.2 (Cross-pollination)", "Exogenesis: Symphony, Pt.3 (Redemption)"],
+		"length":["5:02", "5:46", "3:56", "5:47", "4:13", "6:54", "4:06", "5:38", "4:18", "3:56", "4:37"]
+	},
+	{
+		"name":"Berlin Calling",
+		"artist":"Paul Kalkbrenner",
+		"song":["Aaron", "Queer Fellow", "Azure", "Sky and Sand (Radio Edit)", "Square 1", "Altes Kamuffel (Berlin Calling Edit)", "Torted", "Moob", "Mango (Berlin Calling Edit)", "Atzepeng (Berlin Calling Edit)", "Castenets (Berlin Calling Edit)", "Revolte", "Bengang", "Peet", "Absynthe", "Gebrünn Gebrünn (Berlin Calling Edit)"],
+		"length":["6:00", "4:16", "6:11", "3:59", "6:43", "4:35", "5:29", "3:38", "4:32", "3:58", "5:04", "2:40", "5:37", "3:33", "5:37", "7:14"]
 	}
 ])
 
